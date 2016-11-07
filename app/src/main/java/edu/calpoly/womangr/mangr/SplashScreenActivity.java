1458b3106ba65e9dfbad82128aced370f99d2b23
chesharon <sharonche618@gmail.com>
2016-11-06 16:20:43 -0800

    Changed color scheme and added SplashScreenActivity

---
+++ app/src/main/java/edu/calpoly/womangr/mangr/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package edu.calpoly.womangr.mangr;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int TIME = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, PreferenceActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, TIME);
+    }
+}
