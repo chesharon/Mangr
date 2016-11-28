8ed2eef78fa90bc7a89948cb1e3d9cf93a5594ae
emdwoo <emdwoo@gmail.com>
2016-11-27 22:06:07 -0800

    Add SqlMangaModel

---
+++ app/src/main/java/edu/calpoly/womangr/mangr/sqlite/SqlMangaModel.java
@@ -0,0 +1,117 @@
+package edu.calpoly.womangr.mangr.sqlite;
+
+public class SqlMangaModel {
+    private int id;
+    private String name;
+    private String href;
+    private String author;
+    private String artist;
+    private String status;
+    private String genres;
+    private String info;
+    private String cover;
+
+    // Empty constructor
+    public SqlMangaModel() {
+
+    }
+
+    // Constructor
+    public SqlMangaModel(int id, String name, String href, String author, String artist, String status,
+                         String genres, String info, String cover) {
+        this.id = id;
+        this.name = name;
+        this.href = href;
+        this.author = author;
+        this.artist = artist;
+        this.status = status;
+        this.genres = genres;
+        this.info = info;
+        this.cover = cover;
+    }
+
+    // ID-less constructor
+    public SqlMangaModel(String name, String href, String author, String artist, String status,
+                         String genres, String info, String cover) {
+        this.name = name;
+        this.href = href;
+        this.author = author;
+        this.artist = artist;
+        this.status = status;
+        this.genres = genres;
+        this.info = info;
+        this.cover = cover;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHref() {
+        return this.href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+}
